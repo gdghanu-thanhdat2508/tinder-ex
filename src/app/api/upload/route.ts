d8d2ffb6995f64fe4b87969e3179861844bd68a2
gdghanu-thanhdat2508 <gdschanu.datnt2508@gmail.com>
2025-06-06 22:44:59 +0700

    a

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { put } from "@vercel/blob";
 import { Buffer } from "buffer";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function POST(req: Request) {
     try {
